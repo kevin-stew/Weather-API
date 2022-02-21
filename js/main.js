7c53410ff8bbdb9508c5d40338802c9206cf775f
Kevin Stewart <94493923+kevin-stew@users.noreply.github.com>
2022-02-21 09:36:02 -0600

    seach works with press enter button now

--- js/main.js
+++ js/main.js
@@ -1,33 +1,17 @@
-// Grabbing input from html input form   
+// get input from html form   
 const form = document.querySelector('#citySearch')
 
 //  Event Listener for text form input   
 form.addEventListener("submit", ( event ) => {
-    
     event.preventDefault();
 
     var city1 = document.querySelector('#city').value
-    // console.log(event)
-    // console.log(city1)
-    loadData(city1)
-
-    // ENTER KEY input code below... worked on Friday 2/18, somehow doesn't work now :(
-
-    // var city1 = document.querySelector('#city').value
-    // console.log(city1)
-    // console.log(event)
-    
-    // if (event.keyCode === 13) {
-    //     event.preventDefault();
-    //     loadData(city1)
-    // }
-    
+    loadData(city1)    
 })
 
+// get json data from website  
 const getData = async ( city3 ) => {   
-    // console.log(city3)
     let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city3}&appid=74a8cc2fd08f431fcce5495540770c98`) 
-    // console.log(response.data)
     return response.data
 }
 
@@ -36,13 +20,9 @@ const getData = async ( city3 ) => {
 const loadData = async (city2) => {
 
     const weather = await getData(city2);
-    // console.log(weather)
 
     let cityname = weather.name
-    // console.log(cityname)
-    
     let country = weather.sys.country
-    // console.log(country)
     
     document.querySelector("#city_country").innerText = cityname + ', ' + country
     
